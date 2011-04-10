916f33924e493c9be8290d0aadc35cf85d6899af
Adam Megacz <megacz@cs.berkeley.edu>
2011-04-10 19:37:59 +0000

    All.v: uncomment things

--- src/All.v
+++ src/All.v
@@ -1,9 +1,7 @@
 Require Import ExtractionMain.
-(*
 Require Import ProgrammingLanguageGeneralizedArrow.
 Require Import ProgrammingLanguageFlattening.
 Require Import ProgrammingLanguageArrow.
 Require Import ProgrammingLanguageReification.
-*)
 Require Import ReificationsIsomorphicToGeneralizedArrows.
 
